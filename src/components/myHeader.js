a20ef1d463373beabdfd92500479578f77963460
mpaldum1 <mpaldum1@etf.unsa.ba>
2020-04-09 15:54:14 +0200

    Ostavi VI radi ovo

--- src/components/myHeader.js
+++ src/components/myHeader.js
@@ -1,84 +1,15 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { Link } from "react-router-dom";
-
-import {
-    CaretDownOutlined,
-    MailOutlined,
-    PrinterOutlined,
-    ScanOutlined,
-    TeamOutlined,
-    WechatFilled,
-    HomeOutlined,
-    StarOutlined,
-    ApartmentOutlined
-} from '@ant-design/icons'
+import { Layout } from 'antd';
+import Navbar from './navbar'
 
 const { Header } = Layout;
-const { SubMenu } = Menu;
 
 const MyHeader = () => {
     return (
-        <Header id="header" style = {{}} >
-            <div className="logo" />
+        <Header id="header" style={{ verticalAlign: 'middle'}} >
 
-            <Menu mid="menu" theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Navbar/>
 
-                <Menu.Item id="menuItem" key="1">
-                    <Link to="/">
-                        Naslovna
-                    </Link>
-                </Menu.Item>
-
-                <SubMenu 
-                    key="sub1"
-                    title={
-                        <span>
-                            <span> Proizvodi </span>
-                            <CaretDownOutlined />
-                        </span>
-                    }
-                >
-                    <Menu.Item id="submenuItem" key="11"> <ApartmentOutlined /> Multifunkcionalni sistemi</Menu.Item>
-                    <Menu.Item id="submenuItem" key="12"> <PrinterOutlined /> Printeri</Menu.Item>
-                    <Menu.Item id="submenuItem" key="13"> <ScanOutlined /> Skeneri</Menu.Item>
-                </SubMenu>
-
-                <Menu.Item id="menuItem" key="2">
-                    <Link to="/support">
-                        Podrška
-                    </Link>
-                </Menu.Item>
-
-                <SubMenu
-                    key="sub2"
-                    title={
-
-                        <span>
-                            <span> O nama </span>
-                            <CaretDownOutlined />
-                        </span>
-                    }
-                >
-                    <Menu.Item id="submenuItem" key="15"> <HomeOutlined /> Profil</Menu.Item>
-                    <Menu.Item id="submenuItem" key="16"> <TeamOutlined /> Partneri</Menu.Item>
-                    <Menu.Item id="submenuItem" key="17"> <StarOutlined /> Karijera</Menu.Item>
-                    <Menu.Item id="submenuItem" key="18"> <WechatFilled /> Novosti</Menu.Item>
-                    <Menu.Item id="submenuItem" key="29"> <MailOutlined /> Kontakt</Menu.Item>
-                </SubMenu>
-
-                <Menu.Item id="menuItem" key="5">
-                    <Link to="/references">
-                        Reference
-                    </Link>
-                </Menu.Item>
-
-                <Menu.Item id="menuItem" key="6">
-                    <Link to="/help">
-                        Pomoć
-                    </Link>
-                </Menu.Item>
-            </Menu>
         </Header>
     )
 }
--- src/components/partneri.js
+++ src/components/partneri.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Xerox from '../res/xeroxBP.jpg';
 import Canon from '../res/canonBP.jfif';
+import Kyocera from '../res/kyoceraBP.png';
 
 
 const Partneri = () => {
     return (
         <div style={{ textAlign: 'center', padding: '10px', marginTop: '10px' }}>
+            
             <h1> NAŠI PARTNERI </h1>
 
             <div id="partneri">
@@ -13,9 +15,15 @@ const Partneri = () => {
                     <img src={Xerox} alt="Xerox" />
                 </a>
 
+                <a href ="https://europe.kyocera.com/" target="_blank">
+                <img src={Kyocera} alt="Kyocera" />
+                </a>
+
                 <a href ="https://www.canon.ba/" target="_blank">
                 <img src={Canon} alt="Canon" />
                 </a>
+
+               
 
             </div>
 
