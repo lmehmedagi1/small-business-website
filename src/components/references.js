fbecb506b128dddf628770b69144d05f2f9099ab
mpaldum1 <mpaldum1@etf.unsa.ba>
2020-04-10 23:37:38 +0200

    References - take one

--- src/components/references.js
+++ src/components/references.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Alert } from 'antd';
+import MyGallery from './myGallery'
 
 const References = () => {
     return (
-        <div>
-            MALO NEŠTO O NAŠIM REFERENCAMAAAAA
+        <div style = {{marginTop: '15px'}}>
+          <h1 style = {{textAlign: 'center'}}> NAŠE REFERENCE</h1> 
+            <MyGallery/>
         </div>
 
     );
