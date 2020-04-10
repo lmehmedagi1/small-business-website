974e577e47a67ddb8faaca6622082b684c98baf2
mpaldum1 <mpaldum1@etf.unsa.ba>
2020-04-11 00:22:19 +0200

    Added about page

--- src/components/about.js
+++ src/components/about.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <myMap/>
-            MALO NEŠTO O NAMA
+        <div id = "container">
+
+            <h1> NAŠ PROFIL</h1>
+            <p>
+                Kao potreba za pružanjem što kvalitetnije usluge na polju informacijskih tehnologija, 2009 godine pokrenuli smo kompaniju FACIT d.o.o. Snagom volje i željom za uspjehom od male firme uspjeli smo pozicionirati na tržištu bitan integrator inovativnih tehnologija od komercijalnog sektora preko servisa i tehničke podrške do specijaliziranih uslužnih djelatnosti printanja i knjižare. Zahvaljujući ljudskom faktoru koji nam pruža stabilnost, sigurnost i snagu a svaki novi uspjeh dodatnu kreativnost i dinamičnost, danas smo postali prepoznatljivi po kvalitetu servisiranja naših kupaca, njihovih zahtjeva i potreba.
+
+                Cilj nam je ostvariti partnerske odnose sa našim kupcima i postati dio njihovog tima. Autorizirani smo prodajni i servisni centar vodećeg proizvođača opreme i naprednih rješenja za proizvodnju i upravljanje dokumentima - Xerox. To nam daje mogućnost da implementiramo inovativna rješenja i nove tehnologije i još više unaprijedimo brigu o našim korisnicima. Također, dio smo "Microsoft Partner Network"-a te svojim kupcima možemo ponuditi najbolja software-ska rješenja iz oblasti operativnih sistema i alata za obradu dokumenata.
+
+                Impresivnom i raznovrsnom ponudom opreme za uredsku i produkcijsku upotrebu možemo odgovoriti i najzahtjevnijim kupcima koji traže optimalni odnos cijene i kvaliteta. Kvalitetna oprema kombinirana sa inovativnim softverskim rješenjima garantuje reduciranje troškova poslovanja u cjelini.Ovakva kombinacija također omogućava virtualno upravljanje dokumentima bilo kojih formata integrirajući ih kroz cijelo preduzeće. Zahvaljujući stalnom tehničkom usavršavanju, naše osoblje je posvećeno zadovoljstvu korisnika kontrolom kvaliteta usluga i konsaltingom o korištenju tehničkih resursa. Uz to veoma smo ponosni na ekspeditivnost kojom kupcima osiguravamo snabdijevanje potrošnim materijalom i papirom.
+
+                Komplementarnost naših usluga i ponude omogućava korisnicima da ostanu fokusirani na vlastitu glavnu djelatnost i postizanje boljih poslovnih rezultata, produktivnosti i efikasnosti. Naša očekivanja su da budemo jedna od najboljih domaćih kompanija u integracijskim ICT rješenjima na BH tržištu, te kompetentan partner u regiji.
+            </p>
+
         </div>
 
     );
--- src/components/menu.js
+++ src/components/menu.js
@@ -25,82 +25,83 @@ class MyMenu extends Component {
         return (
             <div id="menuContainer">
 
-            <div style={{float: 'left', width: '10%', textAlign: 'left'}}> 
-                <img  src={Logo} alt="Logo"/> 
+                <div style={{ float: 'left', width: '10%', textAlign: 'left' }}>
+                    <img src={Logo} alt="Logo" />
+                </div>
+
+                <div style={{ float: 'right', width: '10%', fontSize: '20pt' }}>
+                    <a target="_blank" rel="noopener noreferrer" href="mailto:facit@facit.ba">
+                        <MailOutlined />
+                    </a>
+                </div>
+
+
+
+
+                <Menu mid="menu" theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+
+                    <Menu.Item id="menuItem" key="1">
+                        <Link to="/">
+                            Naslovna
+                </Link>
+                    </Menu.Item>
+
+                    <SubMenu
+                        key="sub1"
+                        title={
+                            <span>
+                                <span> Proizvodi </span>
+                                <CaretDownOutlined />
+                            </span>
+                        }
+                    >
+                        <Menu.Item id="submenuItem" key="11"> <ApartmentOutlined /> Multifunkcionalni sistemi</Menu.Item>
+                        <Menu.Item id="submenuItem" key="12"> <PrinterOutlined /> Printeri</Menu.Item>
+                        <Menu.Item id="submenuItem" key="13"> <ScanOutlined /> Skeneri</Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item id="menuItem" key="2">
+                        <Link to="/support">
+                            Podrška
+                </Link>
+                    </Menu.Item>
+
+                    <SubMenu
+                        key="sub2"
+                        title={
+
+                            <span>
+                                <span> O nama </span>
+                                <CaretDownOutlined />
+                            </span>
+                        }
+                    >
+                        <Menu.Item id="submenuItem" key="15"> <HomeOutlined /> Profil
+                    <Link to="/about"> </Link></Menu.Item>
+                        <Menu.Item id="submenuItem" key="16"> <TeamOutlined /> Partneri</Menu.Item>
+                        <Menu.Item id="submenuItem" key="17"> <StarOutlined /> Karijera</Menu.Item>
+                        <Menu.Item id="submenuItem" key="18"> <WechatFilled /> Novosti</Menu.Item>
+                        <Menu.Item id="submenuItem" key="29"> <MailOutlined /> Kontakt</Menu.Item>
+                    </SubMenu>
+
+                    <Menu.Item id="menuItem" key="5">
+                        <Link to="/references">
+                            Reference
+                </Link>
+                    </Menu.Item>
+
+                    <Menu.Item id="menuItem" key="6">
+                        <Link to="/help">
+                            Pomoć
+                </Link>
+                    </Menu.Item>
+
+                </Menu>
+
+
+
+
             </div>
-
-            <div style={{float: 'right', width: '10%', fontSize: '20pt'}}> 
-                <a target="_blank" rel="noopener noreferrer" href="mailto:facit@facit.ba">
-                <MailOutlined />
-                </a>
-            </div>
-                
-
-
-            
-            <Menu mid="menu" theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-
-                <Menu.Item id="menuItem" key="1">
-                    <Link to="/">
-                        Naslovna
-                </Link>
-                </Menu.Item>
-
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-                            <span> Proizvodi </span>
-                            <CaretDownOutlined />
-                        </span>
-                    }
-                >
-                    <Menu.Item id="submenuItem" key="11"> <ApartmentOutlined /> Multifunkcionalni sistemi</Menu.Item>
-                    <Menu.Item id="submenuItem" key="12"> <PrinterOutlined /> Printeri</Menu.Item>
-                    <Menu.Item id="submenuItem" key="13"> <ScanOutlined /> Skeneri</Menu.Item>
-                </SubMenu>
-
-                <Menu.Item id="menuItem" key="2">
-                    <Link to="/support">
-                        Podrška
-                </Link>
-                </Menu.Item>
-
-                <SubMenu
-                    key="sub2"
-                    title={
-
-                        <span>
-                            <span> O nama </span>
-                            <CaretDownOutlined />
-                        </span>
-                    }
-                >
-                    <Menu.Item id="submenuItem" key="15"> <HomeOutlined /> Profil</Menu.Item>
-                    <Menu.Item id="submenuItem" key="16"> <TeamOutlined /> Partneri</Menu.Item>
-                    <Menu.Item id="submenuItem" key="17"> <StarOutlined /> Karijera</Menu.Item>
-                    <Menu.Item id="submenuItem" key="18"> <WechatFilled /> Novosti</Menu.Item>
-                    <Menu.Item id="submenuItem" key="29"> <MailOutlined /> Kontakt</Menu.Item>
-                </SubMenu>
-
-                <Menu.Item id="menuItem" key="5">
-                    <Link to="/references">
-                        Reference
-                </Link>
-                </Menu.Item>
-
-                <Menu.Item id="menuItem" key="6">
-                    <Link to="/help">
-                        Pomoć
-                </Link>
-                </Menu.Item>
-
-            </Menu>
-
-
-            
-
-            </div>
-    );
+        );
     }
 } export default MyMenu;
