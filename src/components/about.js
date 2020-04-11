920f5e4460da5aac2971ff0966bc74cf1b11852f
lmehmedagi1 <lmehmedagi1@etf.unsa.ba>
2020-04-12 01:35:14 +0200

    Nav fix

--- src/components/about.js
+++ src/components/about.js
@@ -25,6 +25,7 @@ const About = () => {
                             
                         <p className="right">
                             <h1> NAŠE PUTOVANJE </h1>
+                            <hr></hr>
                             Kao potreba za pružanjem što kvalitetnije usluge na polju informacijskih tehnologija, 2009. godine pokrenuli smo kompaniju <b>FACIT d.o.o. </b> Snagom volje i željom za uspjehom od male firme uspjeli smo pozicionirati na tržištu bitan integrator inovativnih tehnologija od komercijalnog sektora, preko servisa i tehničke podrške, do specijaliziranih uslužnih djelatnosti printanja i knjižare. Zahvaljujući ljudskom faktoru koji nam pruža stabilnost, sigurnost i snagu a svaki novi uspjeh dodatnu kreativnost i dinamičnost, danas smo postali prepoznatljivi po kvalitetu servisiranja naših kupaca, njihovih zahtjeva i potreba.
 
                        </p>
@@ -40,7 +41,8 @@ const About = () => {
 
                     <p className="left">
                             
-                    <h1> NAŠE CILJ </h1>
+                    <h1> NAŠ CILJ </h1>
+                    <hr></hr>
                         Cilj nam je ostvariti partnerske odnose sa našim kupcima i postati dio njihovog tima. Autorizirani smo prodajni i servisni centar vodećeg proizvođača opreme i naprednih rješenja za proizvodnju i upravljanje dokumentima - <b>Xerox</b>. To nam daje mogućnost da implementiramo inovativna rješenja i nove tehnologije i još više unaprijedimo brigu o našim korisnicima. Također, dio smo <b> "Microsoft Partner Network"-a </b> te svojim kupcima možemo ponuditi najbolja software-ska rješenja iz oblasti operativnih sistema i alata za obradu dokumenata.
                     </p>
                 </div>
@@ -53,6 +55,7 @@ const About = () => {
                     <div className="tekst">
                         <p className="right">
                         <h1> NAŠA PONUDA </h1>
+                        <hr></hr>
                             Impresivnom i raznovrsnom ponudom opreme za uredsku i produkcijsku upotrebu možemo odgovoriti i najzahtjevnijim kupcima koji traže optimalni odnos cijene i kvaliteta. Kvalitetna oprema kombinirana sa inovativnim softverskim rješenjima garantuje reduciranje troškova poslovanja u cjelini. Ovakva kombinacija također omogućava virtualno upravljanje dokumentima bilo kojih formata integrirajući ih kroz cijelo preduzeće. Zahvaljujući stalnom tehničkom usavršavanju, naše osoblje je posvećeno zadovoljstvu korisnika kontrolom kvaliteta usluga i savjetovanjem o korištenju tehničkih resursa. Uz to veoma smo ponosni na ekspeditivnost kojom kupcima osiguravamo snabdijevanje potrošnim materijalom i papirom.
 
                   </p>
@@ -68,7 +71,7 @@ const About = () => {
                         <p className="left">
 
                         <h1> NAŠA OČEKIVANJA </h1>
-
+                        <hr></hr>
                             Komplementarnost naših usluga i ponude omogućava korisnicima da ostanu fokusirani na vlastitu glavnu djelatnost i postizanje boljih poslovnih rezultata, produktivnosti i efikasnosti. Naša očekivanja su da budemo jedna od najboljih domaćih kompanija u integracijskim ICT rješenjima na BH tržištu, te kompetentan partner u regiji.
                  </p>
                     </div>
